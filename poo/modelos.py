13e642bc3d0ccb3b9c4f445c8fae2c8f05051151
Mateus de Sousa Schiavi <mtsschiavi@gmail.com>
2025-09-19 23:49:33 -0300

    first commit

--- poo/modelos.py
+++ poo/modelos.py
@@ -1,16 +1,21 @@
 class Biblioteca:
-    nome = ""
-    ativo = False
+    bibliotecas = []
+    def __init__(self, nome):
+        self.nome = nome
+        self.ativo = False
+        Biblioteca.bibliotecas.append(self)
+        
+    def __str__(self):
+        return self.nome
+    
+    def listar_bibliotecas():
+        for biblioteca in Biblioteca.bibliotecas:
+            print(f"{biblioteca.nome} | {biblioteca.ativo}")
+        
+biblioteca_cidade = Biblioteca("Biblioteca da cidade")
+biblioteca_shopping = Biblioteca("Biblioteca do Shopping")
 
-biblioteca_cidade = Biblioteca()
-biblioteca_cidade.nome = "Biblioteca da Cidade"
-biblioteca_cidade.ativo = True
+print(biblioteca_cidade)
+print(biblioteca_shopping)
 
-biblioteca_shopping = Biblioteca()
-
-bibliotecas = (biblioteca_shopping, biblioteca_cidade)
-print(vars(biblioteca_cidade))
-print(vars(biblioteca_shopping))
-
-for biblioteca in bibliotecas:
-    print(vars(biblioteca))
+Biblioteca.listar_bibliotecas()
