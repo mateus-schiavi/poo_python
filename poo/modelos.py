6a6956f42bdf04989834fc1dd9b5040675e76b39
Mateus de Sousa Schiavi <mtsschiavi@gmail.com>
2025-09-19 23:57:35 -0300

    first commit

--- poo/modelos.py
+++ poo/modelos.py
@@ -2,7 +2,7 @@ class Biblioteca:
     bibliotecas = []
     def __init__(self, nome):
         self.nome = nome
-        self.ativo = False
+        self._ativo = False
         Biblioteca.bibliotecas.append(self)
         
     def __str__(self):
@@ -11,11 +11,19 @@ class Biblioteca:
     def listar_bibliotecas():
         for biblioteca in Biblioteca.bibliotecas:
             print(f"{biblioteca.nome} | {biblioteca.ativo}")
+            
+    def alterna_estado(self):
+        self._ativo = not self._ativo
         
+    @property
+    def ativo(self):
+      return  "ativada" if self._ativo else "desativada"
+    
 biblioteca_cidade = Biblioteca("Biblioteca da cidade")
 biblioteca_shopping = Biblioteca("Biblioteca do Shopping")
 
 print(biblioteca_cidade)
-print(biblioteca_shopping)
+biblioteca_cidade.alterna_estado()
+biblioteca_shopping.alterna_estado()
 
 Biblioteca.listar_bibliotecas()
