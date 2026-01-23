e40a95e16020c295e275c7db91c98bf3d09d9bbe
Mateus de Sousa Schiavi <mtsschiavi@gmail.com>
2026-01-23 15:12:47 -0300

    added .txt file

---
+++ secao_2/dados.py
@@ -0,0 +1,11 @@
+#Pythonflix
+name = "Top Gun Maverick"
+year_launch = 2023
+note_movie = 9.5
+plan_included = False
+
+print(f"name: {name}\nyear launch:{year_launch}\nmovie note: {note_movie}\nplan included:{plan_included}")
+print(type(name))
+print(type(year_launch))
+print(type(note_movie))
+print(type(plan_included))
---
+++ secao_2/input.py
@@ -0,0 +1,13 @@
+# Utilizando o Input
+
+name = input("Digite o nome do filme:\n")
+year_launch = int(input("Digite o ano de lançamento do filme:\n"))
+note_movie = float(input("Digite a nota do filme:\n"))
+
+print(name)
+print(year_launch)
+print(note_movie)
+
+print(type(name))
+print(type(year_launch))
+print(type(note_movie))
